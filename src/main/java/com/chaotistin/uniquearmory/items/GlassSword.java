3c88d9b6d21b946ca9b285ee8bea9fe965833d36
K-Ris <chrisdroid93@gmail.com>
2020-04-04 21:13:21 +0200

    Added GlassSword texture

--- src/main/java/com/chaotistin/uniquearmory/items/GlassSword.java
+++ src/main/java/com/chaotistin/uniquearmory/items/GlassSword.java
@@ -5,7 +5,8 @@ import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
 
-public class GlassSword extends SwordItem {
+public class
+GlassSword extends SwordItem {
     public GlassSword() {
         super(ToolMaterialList.GLASS, 3, -2.4f, (new Item.Properties().group(ModSetup.itemGroup)));
         setRegistryName("glasssword");
